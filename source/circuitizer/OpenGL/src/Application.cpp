3ba1a5a71ca259868e046674bd2e6adf83f2bf6d
Rithvik Nishad <rithvikn2001@gmail.com>
2019-12-06 20:31:35 +0530

    [Major] Added GLFW and GLEW

--- source/circuitizer/OpenGL/src/Application.cpp
+++ source/circuitizer/OpenGL/src/Application.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
@@ -18,6 +20,9 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		return -1;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
