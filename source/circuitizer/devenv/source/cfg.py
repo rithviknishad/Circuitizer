601659007c014dee827a08c2ce40f9aa424a8573
AshBlade <jeenamurali100tab@gmail.com>
2019-12-08 09:54:44 +0530

    Massive Code cleaning in libUI & mainUI done

--- source/circuitizer/devenv/source/cfg.py
+++ source/circuitizer/devenv/source/cfg.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+# UI Configuration
 
+# Default window geometery
 GEOMETERY = '1200x700'
 
+# Basic Theme color
 BG_COLOR = '#1E1E1E'
 FG_COLOR = '#969393'
 
+# Section/Panel Theme color
 PANEL_COLOR = '#252526'
 STATUS_COLOR = '#e3a42c'
 BORDER_COLOR = '#302d2d'
@@ -14,6 +17,7 @@ PEN_COLOR = 'blue'
 PEN_SIZE = 40
 CANVAS_COLOR = 'black'
 
+# Resource symbols
 ADD = 'resource/tool/add.png'
 CARD = 'resource/tool/card.png'
 CLOUD = 'resource/tool/cloud.png'
@@ -24,5 +28,6 @@ REFRESH = 'resource/tool/refresh.png'
 SAVE = 'resource/tool/save.png'
 SETTINGS = 'resource/tool/settings.png'
 
+# Path reference
 RESOURCE_PATH = 'resource/'
 COMPONENT_PATH = 'component/'
--- source/circuitizer/devenv/source/libUI.py
+++ source/circuitizer/devenv/source/libUI.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# Circuitizer Component UI
+
 import os
 import sys
 import glob
@@ -7,19 +10,28 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.filedialog as tkdialog
 
+# custom imports
+
 from cfg import *
+
 
 class ComponentEditor:
     def __init__(self, root):
+        # The root reference
         self.root = root
+
+        # Basic Application Meta Data
         self.root.title('Component Editor')
         self.root.geometry("800x500")
         self.root.configure(background=BG_COLOR)
+
+        # The GUI components
         self.tool_bar(root)
         self.content(root)
         self.status_bar(root)
     
     def content(self, root):
+        """The content div UI code"""
         self.frame = tk.Frame(root)
         # The image preview canvas
         self.img_frame = tk.Frame(self.frame)
@@ -66,17 +78,11 @@ class ComponentEditor:
         self.frame.pack(side=tk.TOP, fill=tk.BOTH)
 
     def tool_bar(self, root):
+        """The tool bar UI code"""
         self.frame = tk.Frame(root)
         self.frame.configure(background=TOOL_COLOR, highlightbackground=BORDER_COLOR, highlightcolor=BORDER_COLOR, highlightthickness=1)
 
-        def generate_tools(self, icon, command):
-            self.image = tk.PhotoImage(file=icon)
-            self.open = tk.Button(self.frame, image=self.image, relief=tk.FLAT, compound=tk.LEFT, command=command)
-            self.open.configure(background=TOOL_COLOR, foreground=FG_COLOR)
-            # reference of this image is required otherwise this image is garbage collected
-            self.open.image = self.image
-            self.open.pack(side=tk.LEFT, fill=tk.BOTH, ipadx=5, ipady=5)
-
+        # the add component action which will load the symbol to the image preview div
         def add_component(self):
             img_file = tkdialog.askopenfilename(filetypes=(
                         ("GIF files", "*.gif"),
@@ -89,25 +95,48 @@ class ComponentEditor:
                 self.img.config(image=self.image)
                 self.img.image = self.image
                 print(img_file)
-        generate_tools(self, ADD, lambda: add_component(self))
-        generate_tools(self, CARD, None)
-        generate_tools(self, CLOUD, None)
+
+        def generate_ui(self, icon, command):
+            """Generate the UI with the custom theme"""
+            self.image = tk.PhotoImage(file=icon)
+            self.open = tk.Button(self.frame, image=self.image, relief=tk.FLAT, compound=tk.LEFT, command=command)
+            self.open.configure(background=TOOL_COLOR, foreground=FG_COLOR)
+            # reference of this image is required otherwise this image is garbage collected
+            self.open.image = self.image
+            self.open.pack(side=tk.LEFT, fill=tk.BOTH, ipadx=5, ipady=5)
+
+        # structure of ui
+        for widget in [
+            (ADD, lambda: add_component(self)),
+            (CARD, None), (CLOUD, None)
+        ]:
+            generate_ui(self, icon=widget[0], command=widget[1])
+
         self.frame.pack(fill=tk.X, side=tk.TOP)
     
     def status_bar(self, root):
+        """The status bar UI code"""
+        # render the frame for the status bar
         self.frame = tk.Frame(root)
         self.frame.configure(background=STATUS_COLOR)
 
+        # the status text with some status information
         self.txt = tk.Label(self.frame, text="Latest Version")
         self.txt.configure(background=STATUS_COLOR, foreground='white')
         self.txt.pack(side=tk.LEFT)
 
         self.frame.pack(fill=tk.X, side=tk.BOTTOM)
 
-if __name__ == "__main__":
+
+def main():
+    # Fix text blurry issue in windows 10 due to text scale settings
     if 'win' in sys.platform:
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
-
+    # Init the tkinter GUI process
     root = tk.Toplevel()
+    # Circuitizer Component UI class bind to the root
     ComponentEditor(root)
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
