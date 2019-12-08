e59448400cd0d4bc5cc0e9c22ed6d3fdf7d9f8a5
Rithvik Nishad <rithvikn2001@gmail.com>
2019-12-08 19:35:04 +0530

    Premake Initialize

---
+++ source/circuitizer/premake5.lua
@@ -0,0 +1,108 @@
+workspace "Circuitizer"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Circuitizer"
+		location "Circuitizer"
+		kind "SharedLib"
+		language "C++"
+		
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"%{prj.name}/vendor/spdlog/include"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "10.0.18362.0"
+
+			defines
+			{
+				"CR_PLATFORM_WINDOWS",
+				"CR_BUILD_DLL"
+			}
+
+			postbuildcommands
+			{
+				("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir.. "/Sandbox")
+			}
+
+		filter "configurations:Debug"
+			defines "CR_DEBUG"
+			optimize "On"
+
+		filter "configurations:Release"
+			defines "CR_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "CR_DIST"
+			optimize "On"
+
+
+
+
+project "Sandbox"
+		location "Sandbox"
+		kind "ConsoleApp"
+		language "C++"
+		
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"Circuitizer/vendor/spdlog/include",
+			"Circuitizer/src"
+		}
+
+		links
+		{
+			"Circuitizer"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "10.0.18362.0"
+
+			defines
+			{
+				"CR_PLATFORM_WINDOWS",
+			}
+
+		filter "configurations:Debug"
+			defines "CR_DEBUG"
+			optimize "On"
+
+		filter "configurations:Release"
+			defines "CR_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "CR_DIST"
+			optimize "On"
