628422eec498dbf1dacb9cae2260904069ca506c
AshBlade <jeenamurali100tab@gmail.com>
2019-12-11 19:09:17 +0530

    Added setting dialog basic code

--- source/circuitizer/devenv/source/libUI.py
+++ source/circuitizer/devenv/source/libUI.py
@@ -29,7 +29,7 @@ class ComponentEditor:
         self.tool_bar(root)
         self.content(root)
         self.status_bar(root)
-    
+
     def content(self, root):
         """The content div UI code"""
         self.frame = tk.Frame(root)
@@ -40,40 +40,6 @@ class ComponentEditor:
         self.img.pack(fill=tk.BOTH)
         self.img_frame.pack(side=tk.LEFT)
     
-        # The definitions of components
-        self.detail_frame = tk.Frame(self.frame, background=BORDER_COLOR)
-
-        # Properties of the components are give below after this label
-        self.tmp_frame = tk.Frame(self.detail_frame, background=BORDER_COLOR)
-        self.temp = tk.Label(self.tmp_frame, text="    Properties Panel \t\t\t\t\t\t\n", background=BORDER_COLOR, foreground=FG_COLOR)
-        self.temp.pack(side=tk.LEFT)
-        self.tmp_frame.pack(side=tk.TOP)
-
-        # Name of the symbols
-        self.name_frame = tk.Frame(self.detail_frame, background=BORDER_COLOR)
-        self.temp = tk.Label(self.name_frame, text="    Component \t\t", background=BORDER_COLOR, foreground='white')
-        self.temp.pack(side=tk.LEFT)
-        self.name = tk.Entry(self.name_frame, width=30)
-        self.name.pack(side=tk.RIGHT)
-        self.name_frame.pack(side=tk.TOP, pady=4)
-
-        # Placeholder of the symbols
-        self.placeholder_frame = tk.Frame(self.detail_frame, background=BORDER_COLOR)
-        self.temp = tk.Label(self.placeholder_frame, text="    Placeholder\t\t", background=BORDER_COLOR, foreground='white')
-        self.temp.pack(side=tk.LEFT)
-        self.placeholder = tk.Entry(self.placeholder_frame, width=30)
-        self.placeholder.pack(side=tk.RIGHT)
-        self.placeholder_frame.pack(side=tk.BOTTOM, pady=4)
-
-        # Max Volt of the symbols
-        self.max_volt_frame = tk.Frame(self.detail_frame, background=BORDER_COLOR)
-        self.temp = tk.Label(self.max_volt_frame, text="    Max Volt\t\t", background=BORDER_COLOR, foreground='white')
-        self.temp.pack(side=tk.LEFT)
-        self.max_volt = tk.Entry(self.max_volt_frame, width=30)
-        self.max_volt.pack(side=tk.RIGHT)
-        self.max_volt_frame.pack(side=tk.BOTTOM, pady=4)
-
-        self.detail_frame.pack(side=tk.LEFT)
         self.frame.configure(background=BORDER_COLOR, highlightbackground=BORDER_COLOR, highlightcolor=BORDER_COLOR, highlightthickness=1)
         self.frame.pack(side=tk.TOP, fill=tk.BOTH)
 
@@ -113,7 +79,7 @@ class ComponentEditor:
             generate_ui(self, icon=widget[0], command=widget[1])
 
         self.frame.pack(fill=tk.X, side=tk.TOP)
-    
+
     def status_bar(self, root):
         """The status bar UI code"""
         # render the frame for the status bar
